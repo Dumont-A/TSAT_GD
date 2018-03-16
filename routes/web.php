1f0d2016fbb1024ddf2f97cb8ab5a57270581f9f
glpmr_dell <glpmr_dell@hotmail.com>
2018-03-16 16:12:03 +0100

    error route

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,3 @@ Route::get('/', function () {
 
 Route::get('competitions','CompetitionController@index')->name("competitions.index");
 Route::resource('competitions', 'CompetitionController');
-
-<<<<<<< HEAD
-Route::get('front/liens_utiles','FrontController@liens_utiles')->name("front.liens_utiles");
-Route::resource('front', 'FrontController');
-=======
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> e725d1cc5a3347a7c2ef4105ad3fee8e67f372e0
