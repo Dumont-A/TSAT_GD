4828166ec3175f3e4c52625d3226e7899f0ac4f6
Alexandre Dumont <dumontalexandre39@gmail.com>
2018-03-26 11:54:42 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-=======
+
 Route::get('liens_utiles','FrontController@liens_utiles')->name("liens_utiles");
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
->>>>>>> 3587d38cba5af582857467dcac47afd38c6e4b30
+
 Route::get('competitions','CompetitionController@index')->name("competitions.index");
 Route::resource('competitions', 'CompetitionController');
