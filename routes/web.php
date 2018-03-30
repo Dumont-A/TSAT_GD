e6ab8364c88b82143bb7dcaaab3119ea0eef3b10
INFO3 <cavinelodie@gmail.com>
2018-03-30 16:09:30 +0200

    modif admin pour ajout contact + modif erreur login

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,10 @@ Route::get('galerie','FrontController@galerie')->name("galerie");
 //Route::get('devenir_membre', 'InfosPratiquesController@devenir_membre')->name("devenir_membre");
 
 // Routes pour le Back-office
-Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
-    
+Route::group(['prefix' => 'admin','middleware'=>'admin'], function() {
+
     Route::get('/laravel-filemanager', '\Unisharp\Laravelfilemanager\controllers\LfmController@show');
-    
+
     Route::get('/', function () {
                 return view('admin.page.dashboard');
             })
